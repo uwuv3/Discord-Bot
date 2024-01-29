14de48ae78798920a3e6ceca13c2bfe0e9e4154f
uwuv3 <a1bilg1sayar@outlook.com>
2024-01-29 09:58:59 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 app.get("*", (r, rs) => rs.send("Naber"));
+app.get("/", (r, rs) => rs.send("Naber"));
 app.post("*", (r, rs) => rs.send("Naber"));
 app.listen(3000,()=>{
     console.log("http://localhost:3000")
 });
-require("./app")
+//require("./app")
